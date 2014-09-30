6976cc02c345e3806be15938ab3bebee0131144a
KathrineGreenberg <kathrinegreenberg@gmail.com>
2014-09-30 11:33:25 -0400

    Changed the classes, edited the js.
    Added classes sales1 & 2, sales__details1 & 2. edited the js so that
    these classes were used.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,30 @@
-var sales = $('.sales'); 
-var sales__details = $('.sales__details');
+var salesA = $('.sales1');
+var salesB = $('.sales2');
+ 
+var salesDetailsA = $('.sales__details1');
+var salesDetailsB = $('.sales__details2');
+
+var salesClickA = function () {
+	
+	var isActive = salesDetailsA.attr('data-state');
+	
+	if (isActive == 'active') {
+		salesDetailsA.attr('data-state', 'inactive');
+	} else {
+		salesDetailsA.attr('data-state', 'active');
+	}
+};
+
+var salesClickB = function () {
+	
+	var isActive = salesDetailsB.attr('data-state');
+	
+	if (isActive == 'active') {
+		salesDetailsB.attr('data-state', 'inactive');
+	} else {
+		salesDetailsB.attr('data-state', 'active');
+	}
+};
+
+salesA.on('click', salesClickA);
+salesB.on('click', salesClickB);
